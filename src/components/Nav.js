780daa079c075c0dccf6a9c85e84ded5e08177d4
Omotola Olaoti <omotolaolaoti@gmail.com>
2022-08-19 22:36:05 +0100

    new

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Logo from "./Logo";
 
 function Nav() {
+  const navRef = useRef(null);
+  const [navBg, setNavBg] = useState(false);
+  const handleScroll = () => {
+    if (window.scrollY > 70) {
+      setNavBg(true);
+    } else {
+      setNavBg(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  const [menuClick, setClick] = useState(false);
+  const checkClick = () => {
+    if (menuClick === true) {
+      setClick(false);
+    } else if (menuClick === false) {
+      setClick(true);
+      setNavBg(true);
+    }
+  };
   return (
-    <div className="nav">
-      <Logo />
-      <div className="nav__links">
+    <div className={`nav ${navBg && "navBg"}`} ref={navRef}>
+      <div className={`mobile-nav ${navBg && "mobilebg"}`}>
+        <div className="logo">
+          <Logo />
+        </div>
+        <div
+          className={`menu ${menuClick ? "close" : "show"}`}
+          onClick={checkClick}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      <div className={`nav__links ${menuClick ? "visible" : "hide"}`}>
         <Link className="link" to="/">
           <div>Home</div>
         </Link>
@@ -19,8 +55,9 @@ function Nav() {
         <div>Blog</div>
         <div>About Us</div>
         <div>Contact Us</div>
+        <div className="btn phone">Connect wallet</div>
       </div>
-      <div className="btn">Connect wallet</div>
+      <div className="desktop btn">Connect wallet</div>
     </div>
   );
 }
