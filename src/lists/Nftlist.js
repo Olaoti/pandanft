f481dad57ee5ba10876135ee76460046ad204667
Omotola Olaoti <omotolaolaoti@gmail.com>
2022-08-23 23:44:15 +0100

    new

--- src/lists/Nftlist.js
+++ src/lists/Nftlist.js
@@ -11,15 +11,15 @@ import nftimg6 from "../assets/images/NFTimages/nft5.png";
 import nftimg7 from "../assets/images/NFTimages/nft6.png";
 import nftimg8 from "../assets/images/NFTimages/nft7.png";
 import nftimg9 from "../assets/images/NFTimages/nft8.png";
-/*import nftimg10 from "../assets/images/NFTimages/nft9.png";
+import nftimg10 from "../assets/images/NFTimages/nft9.png";
 import nftimg11 from "../assets/images/NFTimages/nft10.png";
 import nftimg12 from "../assets/images/NFTimages/nft11.png";
 import nftimg13 from "../assets/images/NFTimages/nft12.png";
 import nftimg14 from "../assets/images/NFTimages/nft13.png";
 import nftimg15 from "../assets/images/NFTimages/nft14.png";
-import nftimg16 from "../assets/images/NFTimages/nft15.png";
-import nftimg17 from "../assets/images/NFTimages/nft16.png";*/
-
+/*import nftimg16 from "../assets/images/NFTimages/nft15.png";
+import nftimg17 from "../assets/images/NFTimages/nft16.png";
+*/
 import person1 from "../assets/images/peopleimages/person1.png";
 import person2 from "../assets/images/peopleimages/person2.png";
 import person3 from "../assets/images/peopleimages/person3.png";
@@ -30,14 +30,14 @@ import person7 from "../assets/images/peopleimages/person7.png";
 import person8 from "../assets/images/peopleimages/person8.png";
 import person9 from "../assets/images/peopleimages/person9.png";
 import { useEffect } from "react";
-/*import person10 from "../assets/images/peopleimages/person10.png";
+import person10 from "../assets/images/peopleimages/person10.png";
 import person11 from "../assets/images/peopleimages/person11.png";
 import person12 from "../assets/images/peopleimages/person12.png";
 import person13 from "../assets/images/peopleimages/person13.png";
-import person14 from "../assets/images/peopleimages/person14.png";*/
+import person14 from "../assets/images/peopleimages/person14.png";
 
 function Nftlist({ checkList, random, btnCheck }) {
-  const [lists, setLists] = useState([
+  const lists = [
     {
       id: 1,
       nftima: nftimg1,
@@ -47,7 +47,7 @@ function Nftlist({ checkList, random, btnCheck }) {
       manimg: person1,
       nftprice: 3.54,
       nftpos: "Hot",
-      nftgenre: "music, virtual, videos",
+      nftgenre: "virtual, videos",
       nftdesc:
         "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
       contact: "0x49cf...a28b",
@@ -62,7 +62,7 @@ function Nftlist({ checkList, random, btnCheck }) {
       manimg: person2,
       nftprice: 8.64,
       nftpos: "Hot",
-      nftgenre: "arts, sports, videos",
+      nftgenre: "arts, videos",
       nftdesc:
         "NFTs (non-fungible tokens) are unique cryptographic tokens that exist on a blockchain and cannot be replicated. NFTs can represent real-world items like artwork and real estate. these real-world tangible assets makes buying, selling, and trading them more efficient while reducing the probability of fraud",
       contact: "0b49cf...a988b",
@@ -77,7 +77,7 @@ function Nftlist({ checkList, random, btnCheck }) {
       manimg: person3,
       nftprice: 7.64,
       nftpos: "live",
-      nftgenre: "arts, sports, virtual",
+      nftgenre: "arts, sports",
       nftdesc:
         "Top Shot is an NFT marketplace where basketball fans can buy, sell, and trade NBA moments. So far, the most expensive collectible traded is LeBron James dunking against the Houston Rockets, which was sold for over $387,000",
       contact: "9o35cf...a988b",
@@ -107,7 +107,7 @@ function Nftlist({ checkList, random, btnCheck }) {
       manimg: person5,
       nftprice: 7.98,
       nftpos: "live",
-      nftgenre: "sports, virtual, videos",
+      nftgenre: "virtual, videos",
       nftdesc:
         "NFT means non-fungible tokens (NFTs), which are generally created using the same type of programming used for cryptocurrencies. In simple terms ...",
       contact: "9o9fg...a987d",
@@ -172,6 +172,90 @@ function Nftlist({ checkList, random, btnCheck }) {
         "Top Shot is an NFT marketplace where basketball fans can buy, sell, and trade NBA moments. So far, the most expensive collectible traded is LeBron James dunking against the Houston Rockets, which was sold for over $387,000",
       contact: "9287fg...a987d",
       lastbid: 7.1,
+    },
+    {
+      id: 10,
+      nftima: nftimg10,
+      nftname: "Krybtal",
+      timeleft: 44,
+      manname: "Koko Banks",
+      manimg: person10,
+      nftprice: 6.77,
+      nftgenre: "videos, music",
+      nftdesc:
+        "T means non-fungible tokens (NFTs), which are generally created using the same type of programming used for cryptocurrencies. In simple term",
+      contact: "9287fg...a987d",
+      lastbid: 4.5,
+    },
+    {
+      id: 11,
+      nftima: nftimg11,
+      nftname: "kobinat",
+      timeleft: 23,
+      manname: "Tall Frayo",
+      manimg: person11,
+      nftprice: 5.6,
+      nftgenre: "sports, music",
+      nftdesc:
+        "T means non-fungible tokens (NFTs), which are generally created using the same type of programming used for cryptocurrencies. In simple term",
+      contact: "9287fg...a987d",
+      lastbid: 4.8,
+    },
+    {
+      id: 12,
+      nftima: nftimg12,
+      nftname: "Cronbayot",
+      timeleft: 32,
+      manname: "Susanne Mar",
+      manimg: person12,
+      nftprice: 4.5,
+      nftgenre: "arts, virtual",
+      nftdesc:
+        "Top Shot is an NFT marketplace where basketball fans can buy, sell, and trade NBA moments. So far, the most expensive collectible traded is LeBron James dunking against the Houston Rockets, which was sold for over $387,000",
+      contact: "9287fg...a987d",
+      lastbid: 9.8,
+    },
+    {
+      id: 13,
+      nftima: nftimg13,
+      nftname: "Lighter",
+      timeleft: 43,
+      manname: "Thronlyn Bay",
+      manimg: person13,
+      nftprice: 5.4,
+      nftgenre: "sports, vidoes",
+      nftdesc:
+        "Top Shot is an NFT marketplace where basketball fans can buy, sell, and trade NBA moments. So far, the most expensive collectible traded is LeBron James dunking against the Houston Rockets, which was sold for over $387,000",
+      contact: "9287fg...a987d",
+      lastbid: 4.1,
+    },
+    {
+      id: 14,
+      nftima: nftimg14,
+      nftname: "Chronc",
+      timeleft: 34,
+      manname: "justin Rye",
+      manimg: person14,
+      nftprice: 9.6,
+      nftgenre: "arts, videos, music",
+      nftdesc:
+        "Top Shot is an NFT marketplace where basketball fans can buy, sell, and trade NBA moments. So far, the most expensive collectible traded is LeBron James dunking against the Houston Rockets, which was sold for over $387,000",
+      contact: "9287fg...a987d",
+      lastbid: 8.3,
+    },
+    {
+      id: 15,
+      nftima: nftimg15,
+      nftname: "Broauth",
+      timeleft: 22,
+      manname: "Rodrib Mark",
+      manimg: person6,
+      nftprice: 14,
+      nftgenre: "music, virtual, sports",
+      nftdesc:
+        "Top Shot is an NFT marketplace where basketball fans can buy, sell, and trade NBA moments. So far, the most expensive collectible traded is LeBron James dunking against the Houston Rockets, which was sold for over $387,000",
+      contact: "9287fg...a987d",
+      lastbid: 10.2,
     } /*{
     id:,
     nftima: nftimg1,
@@ -185,125 +269,8 @@ function Nftlist({ checkList, random, btnCheck }) {
     nftdesc: ,
     contact: ,
     lastbid: ,
-  },{
-    id:,
-    nftima: nftimg1,
-    nftname: "",
-    timeleft: ,
-    manname: ,
-    manimg: person1,
-    nftprice: ,
-    nftpos: ,
-    nftgenre: ,
-    nftdesc: ,
-    contact: ,
-    lastbid: ,
-  },{
-    id:,
-    nftima: nftimg1,
-    nftname: "",
-    timeleft: ,
-    manname: ,
-    manimg: person1,
-    nftprice: ,
-    nftpos: ,
-    nftgenre: ,
-    nftdesc: ,
-    contact: ,
-    lastbid: ,
-  },{
-    id:,
-    nftima: nftimg1,
-    nftname: "",
-    timeleft: ,
-    manname: ,
-    manimg: person1,
-    nftprice: ,
-    nftpos: ,
-    nftgenre: ,
-    nftdesc: ,
-    contact: ,
-    lastbid: ,
-  },{
-    id:,
-    nftima: nftimg1,
-    nftname: "",
-    timeleft: ,
-    manname: ,
-    manimg: person1,
-    nftprice: ,
-    nftpos: ,
-    nftgenre: ,
-    nftdesc: ,
-    contact: ,
-    lastbid: ,
-  },{
-    id:,
-    nftima: nftimg1,
-    nftname: "",
-    timeleft: ,
-    manname: ,
-    manimg: person1,
-    nftprice: ,
-    nftpos: ,
-    nftgenre: ,
-    nftdesc: ,
-    contact: ,
-    lastbid: ,
-  },{
-    id:,
-    nftima: nftimg1,
-    nftname: "",
-    timeleft: ,
-    manname: ,
-    manimg: person1,
-    nftprice: ,
-    nftpos: ,
-    nftgenre: ,
-    nftdesc: ,
-    contact: ,
-    lastbid: ,
-  },{
-    id:,
-    nftima: nftimg1,
-    nftname: "",
-    timeleft: ,
-    manname: ,
-    manimg: person1,
-    nftprice: ,
-    nftpos: ,
-    nftgenre: ,
-    nftdesc: ,
-    contact: ,
-    lastbid: ,
-  },{
-    id:,
-    nftima: nftimg1,
-    nftname: "",
-    timeleft: ,
-    manname: ,
-    manimg: person1,
-    nftprice: ,
-    nftpos: ,
-    nftgenre: ,
-    nftdesc: ,
-    contact: ,
-    lastbid: ,
-  },{
-    id:,
-    nftima: nftimg1,
-    nftname: "",
-    timeleft: ,
-    manname: ,
-    manimg: person1,
-    nftprice: ,
-    nftpos: ,
-    nftgenre: ,
-    nftdesc: ,
-    contact: ,
-    lastbid: ,
   },*/,
-  ]);
+  ];
   const [view, setView] = useState(true);
   const newLists = lists?.filter((list) => {
     return checkList && list.nftpos === checkList;
@@ -314,65 +281,18 @@ function Nftlist({ checkList, random, btnCheck }) {
   const viewClick = () => {
     setView(!view);
   };
-
-  /*const artclick = () => {
-    setLists(
-      lists.filter((list) => {
-        return list.nftgenre.includes("art");
-      })
-    );
-    return lists;
+  const [selected, setSelected] = useState("");
+  const [filterednft, setFilterednft] = useState();
+  const filterClick = (e) => {
+    setSelected(e.target.id);
   };
-  const sportsclick = () => {
-    setLists(
-      lists.filter((list) => {
-        return list.nftgenre.includes("sports");
-      })
-    );
-    return lists;
-  };
-  const musicclick = () => {
-    setLists(
-      lists.filter((list) => {
-        return list.nftgenre.includes("music");
-      })
-    );
-    return lists;
-  };
-  const virtualclick = () => {
-    setLists(
-      lists.filter((list) => {
-        return list.nftgenre.includes("virtual");
-      })
-    );
-    return lists;
-  };
-  const videosclick = () => {
-    setLists(
-      lists.filter((list) => {
-        return list.nftgenre.includes("videos");
-      })
-    );
-    return lists;
-  };*/
-  /*const [selected, setSelected] = useState("");
-
-  const handleClick = (e) => {
-    const clickedBtn = e.target.id;
-    console.log(clickedBtn);
-    if (clickedBtn === selected) {
-      setSelected("");
-    } else {
-      setSelected(clickedBtn);
-    }
-  };
-
   useEffect(() => {
-    var filtered = lists.filter((list) => {
-      return list.nftgenre.includes(selected);
-    });
-    setLists(filtered);
-  }, [selected, lists]);*/
+    setFilterednft(
+      lists?.filter((list) => {
+        return list.nftgenre.includes(selected);
+      })
+    );
+  }, [selected]);
 
   /*click to show or hide sort by options*/
   const [showFilter, setShowFilter] = useState(false);
@@ -398,8 +318,9 @@ function Nftlist({ checkList, random, btnCheck }) {
             </div>
             <div
               className={`flexbox ${showFilter ? "showfilter" : "hidefilter"}`}
+              onClick={filterClick}
             >
-              <div className="btn" id="all">
+              <div className="btn" id=" ">
                 All
               </div>
               <div className="btn" id="arts">
@@ -420,9 +341,9 @@ function Nftlist({ checkList, random, btnCheck }) {
             </div>
           </div>
           {view ? (
-            <NFTcard nftlist={lists.filter((list) => list.id <= 6)} />
+            <NFTcard nftlist={filterednft?.filter((list) => list.id <= 6)} />
           ) : (
-            <NFTcard nftlist={lists} />
+            <NFTcard nftlist={filterednft} />
           )}
           <div className="btn view" onClick={viewClick}>
             {view ? "View More" : "View Less"}
